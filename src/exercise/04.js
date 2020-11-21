e415326db628e678194e758d4592695eec9752d7
Napoleon Salazar <nsalazar@coolfiresolutions.com>
2020-11-20 20:41:47 -0600

    Finish useState tic tac toe (Including extra credit)

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -2,45 +2,12 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import * as React from 'react'
+import {useLocalStorageState} from '../utils'
 
-function Board() {
-  // 🐨 squares is the state for this component. Add useState for squares
-  const squares = Array(9).fill(null)
-
-  // 🐨 We'll need the following bits of derived state:
-  // - nextValue ('X' or 'O')
-  // - winner ('X', 'O', or null)
-  // - status (`Winner: ${winner}`, `Scratch: Cat's game`, or `Next player: ${nextValue}`)
-  // 💰 I've written the calculations for you! So you can use my utilities
-  // below to create these variables
-
-  // This is the function your square click handler will call. `square` should
-  // be an index. So if they click the center square, this will be `4`.
-  function selectSquare(square) {
-    // 🐨 first, if there's already winner or there's already a value at the
-    // given square index (like someone clicked a square that's already been
-    // clicked), then return early so we don't make any state changes
-    //
-    // 🦉 It's typically a bad idea to mutate or directly change state in React.
-    // Doing so can lead to subtle bugs that can easily slip into production.
-    //
-    // 🐨 make a copy of the squares array
-    // 💰 `[...squares]` will do it!)
-    //
-    // 🐨 set the value of the square that was selected
-    // 💰 `squaresCopy[square] = nextValue`
-    //
-    // 🐨 set the squares to your copy
-  }
-
-  function restart() {
-    // 🐨 reset the squares
-    // 💰 `Array(9).fill(null)` will do it!
-  }
-
+function Board({squares, onSquareSelected}) {
   function renderSquare(i) {
     return (
-      <button className="square" onClick={() => selectSquare(i)}>
+      <button className="square" onClick={() => onSquareSelected(i)}>
         {squares[i]}
       </button>
     )
@@ -48,8 +15,6 @@ function Board() {
 
   return (
     <div>
-      {/* 🐨 put the status in the div below */}
-      <div className="status">STATUS</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -65,20 +30,69 @@ function Board() {
         {renderSquare(7)}
         {renderSquare(8)}
       </div>
-      <button className="restart" onClick={restart}>
-        restart
-      </button>
     </div>
   )
 }
 
 function Game() {
+  const [moves, setMoves] = useLocalStorageState('tic-tac-toe:history', [Array(9).fill(null)])
+  const [currentMove, setCurrentMove] = useLocalStorageState('tic-tac-toe:step', 0)
+
+  const squares = moves[currentMove]
+  const nextValue = calculateNextValue(squares)
+  const winner = calculateWinner(squares)
+  const status = calculateStatus(winner, squares, nextValue)
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board squares={squares} onSquareSelected={handleOnSquareSelected} />
+        <button
+          className="restart"
+          onClick={() => {
+            setMoves([Array(9).fill(null)])
+            setCurrentMove(0)
+          }}
+        >
+          restart
+        </button>
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <Moves moves={moves} currentMove={currentMove} onCurrentMoveChanged={setCurrentMove}/>
       </div>
     </div>
+  )
+
+  function handleOnSquareSelected(square) {
+    if (squares[square] || winner) {
+      return
+    }
+    const movesCopy = moves.slice(0, currentMove + 1)
+    const squaresCopy = [...squares]
+    squaresCopy[square] = nextValue
+    movesCopy.push(squaresCopy)
+    setMoves(movesCopy)
+    setCurrentMove(movesCopy.length - 1)
+  }
+}
+
+function Moves({moves, currentMove, onCurrentMoveChanged}) {
+  return (
+    <ol>
+      {moves.map((move, index) => {
+        const message = index === 0 ? `Go to game start` : `Go to move #${index}`
+        return (
+          <li key={`move #${index}`}>
+            { currentMove === index ?
+              <button disabled>{`${message} (current)`}</button>
+              :
+              <button onClick={() => onCurrentMoveChanged(index)}>{message}</button>
+            }
+          </li>
+        )
+      })}
+    </ol>
   )
 }
 
